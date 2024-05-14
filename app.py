5ec4c569411e842d8359cac5a3236e6ba737741a
Marcella Bogatzky <marcella.bogatzky@hotmail.com>
2024-05-13 23:17:42 -0300

    Aula 04

---
+++ app.py
@@ -0,0 +1,16 @@
+from modelos.restaurante import Restaurante #Do arquivo restaurante.app, importar a classe Restaurante
+
+restaurante_gepreto = Restaurante('massas do Gepreto', 'Italiana')
+restaurante_gepreto.receber_avaliacao('Mah', 10)
+restaurante_gepreto.receber_avaliacao('Tuta', 8)
+restaurante_gepreto.receber_avaliacao('Marcella', 6)
+#restaurante_matuta = Restaurante('rancho da Matuta', 'Caipira')
+#restaurante_mirna = Restaurante('peixotescos da Mirna', 'Japonesa')
+
+#restaurante_matuta.alternar_status()
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__' : 
+    main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
 
     restaurantes = [] #Abre uma lista
@@ -6,6 +8,7 @@ class Restaurante:
         self._nome = nome.title() #Mantém a primeira letra em maiúscula
         self._categoria = categoria.upper() #Deixa toda a palavra em caixa alta
         self._ativo = False #O _ antes do ativo indica que se trata de um "atributo privado" e que não deve ser mexido. (O _ não impede de fato que seja feita uma alteração, mas indica)
+        self._avaliacao = []
         Restaurante.restaurantes.append(self) #Adiciona na lista
 
     def __str__(self): # O método especial __str__ transforma a representação e texto de um objeto para o que for definido. (Auxilia a não exibir o endereço de na memória ao pesquisar o objeto no console)
@@ -13,9 +16,9 @@ class Restaurante:
     
     @classmethod #Método exclusivo para a classe
     def listar_restaurantes(cls): #cls é usado por convenção quando se usa método da classe
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes: 
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}') #"str" em "{str(restaurante.media_avaliacoes).ljust(25)}" serve para transformar o resultado número em texto
 
     @property #Método que modifica como um atributo é lido
     def ativo(self):
@@ -24,11 +27,16 @@ class Restaurante:
     def alternar_status(self): #método para os objetos
         self._ativo = not self._ativo
 
-restaurante_gepreto = Restaurante('massas do Gepreto', 'Italiana')
-restaurante_gepreto.alternar_status()
-restaurante_oberyn = Restaurante('foodtruck do Oberyn', 'Hamburgueria')
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
 
-Restaurante.listar_restaurantes()
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1) #Round define a quantidade de casas decimais
+        return media
 
-# print(vars(restaurante_praca)) || vars > define as variáveis e atributos do objeto
-# print(dirs(restaurante_praca)) || dirs > mostra tudo com o que o objeto já "nasce" quando é definido a class
