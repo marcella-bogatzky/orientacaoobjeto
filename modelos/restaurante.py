15ac1201bb5d3964fed43546ea761c1f7c5e1605
Marcella Bogatzky <marcella.bogatzky@hotmail.com>
2024-05-13 22:05:49 -0300

    Aula 02

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,24 @@
 class Restaurante:
-    nome = '' #string
-    categoria = '' #string
-    ativo = False #booleano
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    restaurantes = [] #Abre uma lista
 
-restaurante_pizza = Restaurante()
+    def __init__ (self, nome, categoria): #A função __init__ traz um "método construtor" > instância de um objeto que espera alguma informação. "self" serve para garantirmos que cada restaurante terá suas próprias informações
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False 
+        Restaurante.restaurantes.append(self) #Adiciona na lista
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+    def __str__(self): # O método __str__ transforma a representação e texto de um objeto para o que for definido. (Auxilia a não exibir o endereço de na memória ao pesquisar o objeto no console)
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-print(vars(restaurante_praca))
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+Restaurante.listar_restaurantes()
+
+# print(vars(restaurante_praca)) || vars > define as variáveis e atributos do objeto
+# print(dirs(restaurante_praca)) || dirs > mostra tudo com o que o objeto já "nasce" quando é definido a class
